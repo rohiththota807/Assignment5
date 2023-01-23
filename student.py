96b60c2a85053ee45f992fa745fb83dd5c353eea
rohiththota807 <104708975+rohiththota807@users.noreply.github.com>
2023-01-23 22:09:08 +0530

    Add files via upload

---
+++ student.py
@@ -0,0 +1,14 @@
+class Student:
+    def __init__(self,Name,RollNumber):
+        self.Name=Name
+        self.RollNumber=RollNumber
+
+    def setName(self,Rohith):
+        self.Name=Rohith
+    def getName(self):
+        return self.Name
+    def setRollNumber(self,value):
+        self.setRollNumber=45
+    def getRollNumber(self):
+        return self.RollNumber
+
